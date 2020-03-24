a25b6ba5fa876c86a5916fef42f74f03ccbc6de4
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-03-24 20:32:17 +0900

    [Start]6-6

--- app.js
+++ app.js
@@ -2,8 +2,5 @@
         $('.box1').slideDown(function() {
             $('.box1').css({
                 'background-color': '#0000FF',
-                'width': '200px',
-                'height': '100px'
-            }).slideUp(1000);
         });
     });
