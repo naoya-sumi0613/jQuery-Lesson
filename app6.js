fa264daefd91884724038de23926ecc2e719530a
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-07-23 15:12:54 +0900

    add mouseout

--- app6.js
+++ app6.js
@@ -2,4 +2,7 @@ $(function(){
   $('.box1').mouseover(function(){
     $('.box1').css({'background-color': '#0000ff'});
   });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#ff0000'});
+  });
 });
